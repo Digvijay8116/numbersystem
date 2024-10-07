1445cbc54ca0ab8cce358a82d91f442d3ce6610f
Digvijay8116 <100955521+Digvijay8116@users.noreply.github.com>
2024-10-07 13:58:17 +0530

    mae a function to add al l prime number

--- app.js.js
+++ app.js.js
@@ -2,6 +2,8 @@ const numbers = [
   19, 23, 4, 16, 28, 13, 31, 8, 29, 14, 6, 35, 2, 11, 17, 5, 9, 27, 12, 30,
 ];
 
+let arr2 =[]
+// to find prime number 
 for (let i = 0; i < numbers.length; i++) {
   let count = 0;
 
@@ -12,6 +14,20 @@ for (let i = 0; i < numbers.length; i++) {
   }
 
   if (count < 3) {
+    arr2.push(numbers[i])
     console.log(`${numbers[i]} is a prime number `);
   }
 }
+
+
+
+const findSum = (arr)=>{
+    let sum=0;
+    for (let i=0;i<arr2.length;i++){
+        sum+=arr[i]
+    }
+
+    console.log(`sum of all the prime number is ${sum}`)
+}
+
+findSum(arr2)
